a6cc3d351950a276b0647ee6e1d3a694cc754679
AlTishkov <guslixkanal@gmail.com>
2023-05-19 21:13:24 +0300

    Fragments created

---
+++ app/src/main/java/space/guslix/newsapp/ui/main/MainFragment.kt
@@ -0,0 +1,19 @@
+package space.guslix.newsapp.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import space.guslix.newsapp.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
