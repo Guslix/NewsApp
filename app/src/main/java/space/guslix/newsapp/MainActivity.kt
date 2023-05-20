4ae1236b377478c8624b6edeeb3cdc0028c5062f
AlTishkov <guslixkanal@gmail.com>
2023-05-20 21:09:04 +0300

    Nav menu!!

--- app/src/main/java/space/guslix/newsapp/MainActivity.kt
+++ app/src/main/java/space/guslix/newsapp/MainActivity.kt
@@ -2,15 +2,30 @@ package space.guslix.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import space.guslix.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var _bind: ActivityMainBinding? = null
+    private val bind get() = _bind!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        _bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(bind.root)
+            bind.bottmenu.setupWithNavController(
+                navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+            )
+        }, 5000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _bind=null
     }
 }
